b7ba7e19cc713353225f7ede61307ffe7e783598
dissmaestro <masimka1302@yandex.ru>
2023-10-26 13:52:21 +0500

    new script, gen 20 todo

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 
-COUNT=${1:-10}
-
-# generate one todo
-echo Generating $COUNT
-
-text="asdas"
-
+for i in {1..20}
+do
 curl -X 'POST' \
   'http://localhost:8000/add' \
   -H 'accept: application/json' \
   -H 'Content-Type: application/x-www-form-urlencoded' \
-  -d "task=$text"
+  -d "task=todo-$i"
+done
